735c49c986a7e02a9d7e35f06e4d270747650e62
YakupTopaloglu <112806251+YakupTopaloglu@users.noreply.github.com>
2023-07-27 13:12:44 +0300

    Create Data.py

---
+++ Streamlit/Data.py
@@ -0,0 +1,49 @@
+import sys
+from dronekit import connect, VehicleMode
+import argparse
+import time
+from openpyxl import Workbook,load_workbook
+
+
+def connectMyPlane():
+    parser = argparse.ArgumentParser(description='commands')
+    parser.add_argument('--connect', default='tcp:127.0.0.1:5762')
+    args = parser.parse_args()
+    connection_string = args.connect
+    baud_rate = 57600
+    vehicle = connect(connection_string, baud=baud_rate, wait_ready=True)
+    return vehicle
+
+def function():
+    vehicle = connectMyPlane()
+    vehicle.mode = VehicleMode("AUTO")
+    vehicle.armed = True
+    wb = Workbook()
+    ws = wb.active
+    ws.delete_rows(1, ws.max_row)
+    ws.title = "DATA"
+    ws["A1"] = "SANİYE"
+    ws["B1"] = "HIZ"
+    ws["C1"] = "ALTİTUDE"
+    ws["D1"] = "PİTCH"
+    ws["E1"] = "ROLL"
+    ws["F1"] = "YAW"
+    ws["G1"] = "BATARYA SEVİYESİ"
+    ws["H1"]= "LATİTUDE"
+    ws["I1"]= "LONGİTUDE"
+    saniye = 0
+    while True:
+        saniye = saniye + 1
+        airspeed = vehicle.airspeed
+        altitude = vehicle.location.global_relative_frame.alt
+        batarya_seviye = vehicle.battery.level
+        attitude = vehicle.attitude
+        latitude=vehicle.location.global_frame.lat
+        longitude=vehicle.location.global_relative_frame.lon
+        ws.append([saniye, airspeed, altitude, attitude.pitch, attitude.roll, attitude.yaw, batarya_seviye,latitude,longitude])
+        wb.save("data.xlsx")
+        time.sleep(1)
+
+if __name__ == "__main__":
+    function()
+    print("Started")
